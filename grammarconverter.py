b3c52562fbaab3d039f0125d828ffa649e928c0b
Zeus-s <kristabdixx@gmail.com>
2021-11-20 14:20:22 +0700

    grammarconverter.py

---
+++ grammarconverter.py
@@ -0,0 +1,69 @@
+# Global dictionary used for storing the rules.
+RULE_DICT = {}
+
+
+def read_grammar(grammar_file):
+    """
+    I.S. grammar_file adalah sebuah filename yang ada
+    F.S. Mengembalikan list yang berisi setiap rule yang terpisah
+    """
+    with open(grammar_file) as cfg:
+        lines = cfg.readlines()
+    return [x.replace("->", "").split() for x in lines]
+
+
+def add_rule(rule):
+    """
+    I.S. Rule untuk ditambahkan pada dictionary, mungkin ada
+    F.S. Dictionary telah ditambah sebuah rule
+    """
+    global RULE_DICT
+
+    if rule[0] not in RULE_DICT:
+        RULE_DICT[rule[0]] = []
+    RULE_DICT[rule[0]].append(rule[1:])
+
+
+def convert_grammar(grammar):
+    """
+    I.S. Grammar terdefinisi adalah CFG dari suatu bahasa
+    F.S. Mengembalikan hasil konversi CFG ke CNF (Chowsky Normal Form)
+    """
+    global RULE_DICT
+    unit_productions, cnf = [], []
+    cnf_append = cnf.append
+    idx = 0
+
+    for rule in grammar:
+        new_rules = []
+        if len(rule) == 2 and rule[1][0] != "'":
+            unit_productions.append(rule)
+            add_rule(rule)
+            continue
+        elif len(rule) > 2:
+            # Rule is in form A -> X B C [...] or A -> X a.
+            terminals = [(item, i) for i, item in enumerate(rule) if item[0] == "'"]
+            if terminals:
+                for item in terminals:
+                    rule[item[1]] = f"{rule[0]}{str(idx)}"
+                    new_rules += [f"{rule[0]}{str(idx)}", item[0]]
+                idx += 1
+            while len(rule) > 3:
+                new_rules += [f"{rule[0]}{str(idx)}", rule[1], rule[2]]
+                rule = [rule[0]] + [f"{rule[0]}{str(idx)}"] + rule[3:]
+                idx += 1
+        add_rule(rule)
+        cnf_append(rule)
+        if new_rules:
+            cnf_append(new_rules)
+    while unit_productions:
+        rule = unit_productions.pop()
+        if rule[1] in RULE_DICT:
+            for item in RULE_DICT[rule[1]]:
+                new_rule = [rule[0]] + item
+                if len(new_rule) > 2 or new_rule[1][0] == "'":
+                    cnf_append(new_rule)
+                else:
+                    unit_productions.append(new_rule)
+                add_rule(new_rule)
+    return cnf
